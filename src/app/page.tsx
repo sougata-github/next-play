014d120b596edc46c59c1d10822695281f8753e5
Sougata <sougatadas9874@gmail.com>
2025-02-15 17:58:27 +0530

    add logo and change theme.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div className="text-2xl">App</div>;
+  return (
+    <div className="text-2xl">
+      <Button>Click Me</Button>
+    </div>
+  );
 }
